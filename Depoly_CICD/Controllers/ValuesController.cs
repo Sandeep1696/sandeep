62038ef91892b25a2cbcc7b7a7691663c33da213
san <akurathisandeep@gmail.com>
2024-07-18 09:34:37 +0530

    Code commit to GithHub

---
+++ Depoly_CICD/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Depoly_CICD.Controllers
+{
+    [Route("api/depoly")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Result()
+        {
+            return "Depolyed Successfully";
+        }
+
+
+
+    }
+}
